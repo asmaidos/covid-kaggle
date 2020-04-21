Solve a bug in the similarity computation, compare BM25 / Bert scores.

covid/data_loading.py
@@ -25,6 +25,14 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+from rank_bm25 import BM25Okapi, BM25Plus # don't use BM25L, there is a mistake
+import os
+import torch
+import numpy
+from tqdm import tqdm
+from transformers import *
+import warnings
+
 nltk.download('stopwords')
 nltk.download('punkt')
 
@@ -158,54 +166,6 @@ def clean_text_for_query_search(index, text_list, stopword_remove=False,
     
     return cleaned_doc
 
-# # extract all data
-# data_dir = '/home/jkraft/Dokumente/Kaggle/subset_data/' # 1000 documents
-# all_files = load_files(data_dir)
-# data_df = generate_clean_df(all_files) 
-
-# # prepare text for BM25
-# text_to_clean = list(data_df['text'])
-# index_list = list(range(len(text_to_clean)))
-
-# data_df['cleaned_text'] = list(map(
-#         lambda x: clean_text_for_query_search(x, text_to_clean), trange(len(text_to_clean)))
-#         )
-    
-# # prepare text for BM25
-# abstract_text_to_clean = np.array(data_df['abstract'])
-# # replace NaN for papers without abstracts
-# is_nan_list = list(map(lambda x: str(x), list(abstract_text_to_clean)))
-# abstract_text_to_clean[np.array(is_nan_list) == 'nan'] = ''
-# index_list = list(range(len(abstract_text_to_clean)))
-
-# data_df['cleaned_abstract'] = list(map(
-#         lambda x: clean_text_for_query_search(
-#             x, abstract_text_to_clean), trange(len(abstract_text_to_clean)))
-#         )
-    
-# # prepare text for BM25
-# abstract_text_to_clean = np.array(data_df['title'])
-# # replace NaN for papers without abstracts
-# is_nan_list = list(map(lambda x: str(x), list(abstract_text_to_clean)))
-# abstract_text_to_clean[np.array(is_nan_list) == 'nan'] = ''
-# index_list = list(range(len(abstract_text_to_clean)))
-
-# data_df['cleaned_title'] = list(map(
-#         lambda x: clean_text_for_query_search(
-#             x, abstract_text_to_clean), trange(len(abstract_text_to_clean)))
-#         )
-
-
-# ind_max = 100
-# _ = list(map(
-#         lambda x: clean_text_for_query_search(x, text_to_clean[:ind_max]), trange(ind_max))
-#         )
-
-# # save data
-# data_df.to_csv("./1000_doc_df.csv", index=False)
-    
-	
-import os
 
 	
 os.chdir('/home/jkraft/Dokumente/Kaggle/')
@@ -217,7 +177,7 @@ data_df = pd.read_csv("./1000_doc_df.csv")
 # Try BM25
 # =============================================================================
 
-from rank_bm25 import BM25Okapi, BM25Plus # don't use BM25L, there is a mistake
+
 # in the implementation!
 
 
@@ -277,6 +237,43 @@ def search_corpus_for_question(quest_text, data_df, model=BM25Plus, top_n=10,
 
     return indices, scores, flat_query
 
+# TO DO: try averaging the last 4 layers embeddings
+def extract_scibert(text, tokenizer, model):
+    text_ids = torch.tensor([tokenizer.encode(text, add_special_tokens=True)])
+    text_words = tokenizer.convert_ids_to_tokens(text_ids[0])[1:-1]
+
+    n_chunks = int(numpy.ceil(float(text_ids.size(1)) / 510))
+    states = []
+    class_states = []
+    for ci in range(n_chunks):
+        text_ids_ = text_ids[0, 1 + ci * 510:1 + (ci + 1) * 510]
+        text_ids_ = torch.cat([text_ids[0, 0].unsqueeze(0), text_ids_])
+        if text_ids[0, -1] != text_ids[0, -1]:
+            text_ids_ = torch.cat([text_ids_, text_ids[0, -1].unsqueeze(0)])
+
+        with torch.no_grad():
+            state = model(text_ids_.unsqueeze(0))[0]
+            # stock all embeddings and the CLS embeddings
+            state = state[:, 1:-1, :]
+            class_state = state[:, 0, :]
+        states.append(state)
+        class_states.append(class_state)
+
+    state = torch.cat(states, axis=1)
+    class_state = torch.cat(class_states, axis=1)
+
+    return text_ids, text_words, state[0], class_state[0]
+
+
+def cross_match(state1, state2, use_CLS=False):
+    if not use_CLS:
+        sim = torch.cosine_similarity(torch.mean(state1, 0), torch.mean(state2, 0), dim=0)
+    else:
+        sim = torch.cosine_similarity(state1, state2, dim=0)
+    sim = sim.numpy()
+    return sim
+
+
 # quest_text = read_question(task_text, quest_filename)
 
 # consider only papers about the coronavirus
@@ -304,6 +301,9 @@ quest_text = ['COVID-19 risk factors? epidemiological studies',
  '    Neonates pregnant',
  '    Socio-economic behavioral factors economic impact virus differences']
 
+
+task_text = ['Neonates and pregnant women.']
+
 quest_text = read_question(task_text, quest_filename)
 # remove \n, but has no impact, this is already done somewhere else
 # quest_text = list(map(lambda x: str(x).strip(), quest_text))
@@ -314,62 +314,20 @@ indices, scores, quest = search_corpus_for_question(
 if model == BM25Okapi:
     contain_keyword = np.array(indices)[scores>0]
     answers = data_df_red.iloc[contain_keyword, :].copy()
+    answers['scores_BM25'] = scores[scores>0]
 else:
     answers = data_df_red.iloc[indices, :].copy()
+    answers['scores_BM25'] = scores
+answers = answers.sort_values(['scores_BM25'], ascending=False)
 answers = answers.reset_index(drop=True)
 
-ans = answers[['title', 'abstract']][:20]
+ans = answers[['scores_BM25', 'title', 'abstract', 'text']][:20]
 
 # TO DO: add titles to abstract, as some papers lack abstracts
 
 
-# answers['abstract'][0]
 
-
-# import matplotlib.pyplot as plt
-# # We can set the number of bins with the `bins` kwarg
-# plt.hist(scores, bins=100)
-
-# answers.columns
-
-
-# len(np.array(scores)[scores>0])
-
-
-import torch
-import numpy
-from tqdm import tqdm
-from transformers import *
-import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-
-
-def extract_scibert(text, tokenizer, model):
-    text_ids = torch.tensor([tokenizer.encode(text, add_special_tokens=True)])
-    text_words = tokenizer.convert_ids_to_tokens(text_ids[0])[1:-1]
-
-    n_chunks = int(numpy.ceil(float(text_ids.size(1))/510))
-    states = []
-    class_states = []
-    for ci in range(n_chunks):
-        text_ids_ = text_ids[0, 1+ci*510:1+(ci+1)*510]            
-        text_ids_ = torch.cat([text_ids[0, 0].unsqueeze(0), text_ids_])
-        if text_ids[0, -1] != text_ids[0, -1]:
-            text_ids_ = torch.cat([text_ids_, text_ids[0,-1].unsqueeze(0)])
-        
-        with torch.no_grad():
-            state = model(text_ids_.unsqueeze(0))[0]
-
-            state = state[:, 1:-1, :]
-            class_state = state[:, 0, :]
-        states.append(state)
-        class_states.append(class_state)
-
-    state = torch.cat(states, axis=1)
-    class_state = torch.cat(class_states, axis=1)
-
-    return text_ids, text_words, state[0], class_state[0]
-
 
 tokenizer = AutoTokenizer.from_pretrained('monologg/biobert_v1.1_pubmed', do_lower_case=False)
 model = AutoModel.from_pretrained('monologg/biobert_v1.1_pubmed')
@@ -381,49 +339,60 @@ model = AutoModel.from_pretrained('monologg/biobert_v1.1_pubmed')
 # model = AutoModelWithLMHead.from_pretrained("bert-large-cased")
 
 
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased")
-model = AutoModelWithLMHead.from_pretrained("distilbert-base-cased")
+# tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased")
+# model = AutoModelWithLMHead.from_pretrained("distilbert-base-cased")
+#
+# tokenizer = AutoTokenizer.from_pretrained("albert-xxlarge-v1")
+# model = AutoModelWithLMHead.from_pretrained("albert-xxlarge-v1")
+#
+#
+# tokenizer = AutoTokenizer.from_pretrained("albert-large-v1")
+# model = AutoModelWithLMHead.from_pretrained("albert-large-v1")
+#
+#
 
-tokenizer = AutoTokenizer.from_pretrained("albert-xxlarge-v1")
-model = AutoModelWithLMHead.from_pretrained("albert-xxlarge-v1")
-
-
-tokenizer = AutoTokenizer.from_pretrained("albert-large-v1")
-model = AutoModelWithLMHead.from_pretrained("albert-large-v1")
-
-def cross_match(state1, state2):
-
-    sim = torch.cosine_similarity(torch.mean(state, 0), torch.mean(query_state, 0), dim=0)
-    sim = sim.numpy()
-    return sim
 
 t = time()
 flat_query = ''.join(quest_text)
+
+flat_query = 'COVID-19 risk factors? Data on potential risks factors. Smoking, pre-existing pulmonary disease. Co-infections (determine whether co-existing respiratory / viral infections make the virus more transmissible or virulent) and other co-morbidities. Neonates and pregnant women. Socio-economic and behavioral factors to understand the economic impact of the virus and whether there were differences.'
+
+flat_query = 'COVID-19 risk factors? Neonates and pregnant women.'
+
+flat_query = 'Smoking, pre-existing pulmonary disease'
+
+flat_query = 'Neonates and pregnant women.'
+
 query_ids, query_words, query_state, query_class_state = extract_scibert(flat_query, tokenizer, model)
 print((time()-t)*1000)
 print(len(query_words))
 # print(query_state)
 
-
+# remove rows without titles / abstracts
 ans_red = ans.dropna()
+ans_red = ans_red.reset_index(drop=True)
 sim_scores = []
 
-for text in tqdm(ans_red['title']):
+use_CLS = True
+# compute similarity scores
+for text in tqdm(ans_red['abstract']):
     text_ids, text_words, state, class_state = extract_scibert(text, tokenizer, model)
-    # sim_score = cross_match(query_state, state)
-    sim_score = cross_match(query_class_state, class_state) # try cosine on CLS tokens
+    if use_CLS:
+        sim_score = cross_match(query_class_state, class_state, True) # try cosine on CLS tokens
+    else:
+        sim_score = cross_match(query_state, state, False)
     sim_scores.append(sim_score)
-
-# t = time()
-# text_ids, text_words, state = extract_scibert(end_ans['abstract'][1], tokenizer, model)
-# print((time()-t)*1000)
-# print(len(text_words))
 
 
 # Select the index of top 5 paragraphs with highest relevance
 rel_index = np.flip(numpy.argsort(sim_scores))[:5]
 end_ans = ans_red.iloc[rel_index, :]
 end_ans = end_ans.reset_index(drop=True)
+end_ans['score_ML'] = np.array(sim_scores)[rel_index]
+# reorder columns
+end_ans = end_ans[['score_ML', 'scores_BM25', 'title', 'abstract']]
+
+# TO DO: try more articles
 
 # tokenizer = AutoTokenizer.from_pretrained('openai-gpt')   # Initialize tokenizer
 # model = AutoModelWithLMHead.from_pretrained('openai-gpt')    # Download model and configuration from S3 and cache.